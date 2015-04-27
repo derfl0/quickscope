876f9bb1a3ecc01fd1167b0fb843c8aec95bf744
Florian Bieringer <florian.bieringer@uni-passau.de>
2015-04-27 13:04:38 +0200

    Fix multiple window problem

--- assets/quickscope.js
+++ assets/quickscope.js
@@ -5,28 +5,29 @@ $(document).ready(function () {
         var href = $(this).attr('href');
         var begin = href.indexOf("auswahl=") + 8;
         var id = href.substr(begin, 32);
-        var quickscope = $('div.quickscope[data-quickscope="'+id+'"]');
+        var quickscope = $('div.quickscope[data-quickscope="' + id + '"]');
 
         if (quickscope.length <= 0) {
             var scope = $('<div>').addClass('quickscope').attr('data-quickscope', id);
+            $('body').prepend(scope);
             $.ajax({
-                url: STUDIP.URLHelper.getURL('plugins.php/QuickscopePlugin/show/course/'+id),
+                url: STUDIP.URLHelper.getURL('plugins.php/QuickscopePlugin/show/course/' + id),
                 method: "POST",
                 dataType: 'html'
             }).done(function (data) {
-                $('div.quickscope[data-quickscope="'+id+'"]').replaceWith(data);
-                quickscope = $('div.quickscope[data-quickscope="'+id+'"]');
+                $('div.quickscope[data-quickscope="' + id + '"]').html(data);
+                quickscope = $('div.quickscope[data-quickscope="' + id + '"]');
                 quickscope.css('left', position.left + (width / 2) - (quickscope.width() / 2));
                 quickscope.css('top', position.top - quickscope.outerHeight(true) - 30);
             });
-            $('body').prepend(scope);
         }
-        quickscope.fadeIn(300);
         quickscope.css('left', position.left + (width / 2) - (quickscope.width() / 2));
         quickscope.css('top', position.top - quickscope.outerHeight(true) - 30);
+        quickscope.fadeIn(300);
     });
 
     $('a[href*="seminar_main.php"]').mouseleave(function (e) {
         $('div.quickscope').fadeOut(300);
+        $('div.quickscope.not-loaded').hide();
     });
 });
