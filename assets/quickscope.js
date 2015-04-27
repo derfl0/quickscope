81aec51dc6b08534a78a98d8716d05d32a120ba0
Florian Bieringer <florian.bieringer@uni-passau.de>
2015-04-27 13:24:16 +0200

    Delay calling a bit

--- assets/quickscope.js
+++ assets/quickscope.js
@@ -8,18 +8,20 @@ $(document).ready(function () {
         var quickscope = $('div.quickscope[data-quickscope="' + id + '"]');
 
         if (quickscope.length <= 0) {
-            var scope = $('<div>').addClass('quickscope').attr('data-quickscope', id);
-            $('body').prepend(scope);
-            $.ajax({
-                url: STUDIP.URLHelper.getURL('plugins.php/QuickscopePlugin/show/course/' + id),
-                method: "POST",
-                dataType: 'html'
-            }).done(function (data) {
-                $('div.quickscope[data-quickscope="' + id + '"]').html(data);
-                quickscope = $('div.quickscope[data-quickscope="' + id + '"]');
-                quickscope.css('left', position.left + (width / 2) - (quickscope.width() / 2));
-                quickscope.css('top', position.top - quickscope.outerHeight(true) - 30);
-            });
+            timeout = setTimeout(function () {
+                var scope = $('<div>').addClass('quickscope').attr('data-quickscope', id);
+                $('body').prepend(scope);
+                $.ajax({
+                    url: STUDIP.URLHelper.getURL('plugins.php/QuickscopePlugin/show/course/' + id),
+                    method: "POST",
+                    dataType: 'html'
+                }).done(function (data) {
+                    $('div.quickscope[data-quickscope="' + id + '"]').html(data);
+                    quickscope = $('div.quickscope[data-quickscope="' + id + '"]');
+                    quickscope.css('left', position.left + (width / 2) - (quickscope.width() / 2));
+                    quickscope.css('top', position.top - quickscope.outerHeight(true) - 30);
+                });
+            }, 300);
         }
         quickscope.css('left', position.left + (width / 2) - (quickscope.width() / 2));
         quickscope.css('top', position.top - quickscope.outerHeight(true) - 30);
@@ -27,6 +29,7 @@ $(document).ready(function () {
     });
 
     $('a[href*="seminar_main.php"]').mouseleave(function (e) {
+        clearTimeout(timeout);
         $('div.quickscope').fadeOut(300);
         $('div.quickscope.not-loaded').hide();
     });
