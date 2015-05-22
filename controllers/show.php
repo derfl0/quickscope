c4377f978ade5d706e9378808d67ec1cbd5249c5
Florian Bieringer <florian.bieringer@uni-passau.de>
2015-05-22 09:57:01 +0200

    Quickscope shows collisions

--- controllers/show.php
+++ controllers/show.php
@@ -1,33 +1,44 @@
 <?php
+
 class ShowController extends StudipController {
 
-    public function __construct($dispatcher)
-    {
+    public function __construct($dispatcher) {
         parent::__construct($dispatcher);
         $this->plugin = $dispatcher->plugin;
     }
 
-    public function before_filter(&$action, &$args)
-    {
+    public function before_filter(&$action, &$args) {
         parent::before_filter($action, $args);
         $this->set_layout(null);
         $this->set_content_type('text/html;Charset=windows-1252');
         //$this->set_layout($GLOBALS['template_factory']->open('layouts/base_without_infobox'));
     }
 
-    public function index_action()
-    {
+    public function index_action() {
         $this->answer = 'Yes';
     }
-    
+
     public function course_action($course_id) {
         $this->course = Course::find($course_id);
         $this->sem = new Seminar($this->course);
+
+        // check if we are in that course otherwise query collisions
+        if (!CourseMember::exists($course_id, User::findCurrent()->id)) {
+            $collisionStmt = DBManager::get()->prepare("SELECT seminare.*, count(*) as collisions FROM seminare JOIN seminar_user USING (seminar_id) JOIN termine ON (termine.range_id = seminare.seminar_id) 
+JOIN termine compare ON ((termine.date + 1 BETWEEN compare.date AND compare.end_time) OR (termine.end_time - 1 BETWEEN compare.date AND compare.end_time)) 
+WHERE user_id = ? 
+AND compare.range_id = ? 
+GROUP BY seminar_id");
+            $collisionStmt->execute(array(User::findCurrent()->id, $course_id));
+            while ($data = $collisionStmt->fetch(PDO::FETCH_ASSOC)) {
+                $course = Course::import($data);
+                $this->collisions[$course->getFullname()] = $data['collisions'];
+            }
+        }
     }
 
     // customized #url_for for plugins
-    function url_for($to)
-    {
+    function url_for($to) {
         $args = func_get_args();
 
         # find params
@@ -41,5 +52,6 @@ class ShowController extends StudipController {
         $args[0] = $to;
 
         return PluginEngine::getURL($this->dispatcher->plugin, $params, join('/', $args));
-    } 
+    }
+
 }
--- views/show/course.php
+++ views/show/course.php
@@ -1,12 +1,23 @@
-<?//<div class="quickscope"> ?>
+<? //<div class="quickscope">   ?>
 <img class="quickscope_avatar" src="<?= CourseAvatar::getAvatar($course->id)->getURL(Avatar::MEDIUM) ?>">
 <div class="quickscope_text">
     <h3><?= htmlReady($course->getFullname()) ?></h3>
     <p>
-        <?= join(', ', array_map(function($obj){ return $obj->user->getFullname(); }, $course->getMembersWithStatus('dozent'))) ?>
+        <?=
+        join(', ', array_map(function($obj) {
+                    return $obj->user->getFullname();
+                }, $course->getMembersWithStatus('dozent')))
+        ?>
     </p>
     <p>
-        <?= $sem->getDatesHTML() ?>
+    <?= $sem->getDatesHTML() ?>
     </p>
+        <? if ($collisions): ?>
+            <? foreach ($collisions as $course => $count): ?>
+            <p class="collisions">
+            <?= sprintf(_('%s Überschneidungen mit %s'), $count, $course) ?>
+            </p>
+    <? endforeach; ?>
+<? endif; ?>
 </div>
-<? //</div> ?>
+<? //</div>   ?>
